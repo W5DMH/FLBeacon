ee64f367f52dc0cdcf4a5bd2671bded6d5cd1415
W5DMH <34745389+W5DMH@users.noreply.github.com>
2018-01-14 10:04:39 +0000

    Add files via upload

---
+++ FLBeaconout.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import time
+import six
+
+if six.PY2:
+    import ConfigParser as configparser
+else:
+    import configparser
+import sys
+
+import pyfldigi
+fldigi = pyfldigi.Client(hostname='127.0.0.1', port=7362)  # <-- Constructor call
+#fldigi.rig.frequency = 14074650.000
+
+
+parser = configparser.ConfigParser()
+parser.read('FLBeacon.conf')
+
+#print(parser.get('MessageConfig', 'Callsign1'))
+#print(parser.get('MessageConfig', 'Callsign2'))
+#print(parser.get('MessageConfig', 'Message'))
+#print(parser.getint('MessageConfig', 'timer'))
+
+callsign1Get = (parser.get('MessageConfig', 'Callsign1'))
+callsign2Get = (parser.get('MessageConfig', 'Callsign2'))
+messageGet = (parser.get('MessageConfig', 'message'))
+timerGet = (parser.getint('MessageConfig', 'timer'))
+
+
+
+#newbeaconVal = "./txbeacon.py -c 20 " + callsign2Get + " " + callsign2Get +" DE " + callsign1Get +" "+ callsign1Get +" "+ messageGet
+#print(newbeaconVal)
+messagevar = " " + callsign2Get + " " + callsign2Get +" de " + callsign1Get +" "+ callsign1Get +" "+ messageGet
+print(messagevar)
+fldigi.main.send(messagevar,timeout=45)
+
+
+while True:
+   fldigi.main.send(messagevar,timeout=45)
+   #os.system(messagevar)
+   print("sleeping") 
+   time.sleep(timerGet)
+
+
+
